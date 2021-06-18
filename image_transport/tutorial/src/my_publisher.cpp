1200257a5fed55a4e8f96728fa601fb604aefaf0
Steve Peters <scpeters@openrobotics.org>
2021-06-17 22:31:50 -0700

    Update tutorial code to ros2

--- image_transport/tutorial/src/my_publisher.cpp
+++ image_transport/tutorial/src/my_publisher.cpp
@@ -1,22 +1,25 @@
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "image_publisher");
-  ros::NodeHandle nh;
-  image_transport::ImageTransport it(nh);
+  rclcpp::init(argc, argv);
+  rclcpp::NodeOptions options;
+  rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("image_publisher", options);
+  image_transport::ImageTransport it(node);
   image_transport::Publisher pub = it.advertise("camera/image", 1);
 
   cv::Mat image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
-  sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
+  std_msgs::msg::Header hdr;
+  sensor_msgs::msg::Image::SharedPtr msg;
+  msg = cv_bridge::CvImage(hdr, "bgr8", image).toImageMsg();
 
-  ros::Rate loop_rate(5);
-  while (nh.ok()) {
+  rclcpp::WallRate loop_rate(5);
+  while (rclcpp::ok()) {
     pub.publish(msg);
-    ros::spinOnce();
+    rclcpp::spin_some(node);
     loop_rate.sleep();
   }
 }
